de22da42aee34370ff0a808e734649af6ed7e009
Luiz Felipe <jpgpipinho@gmail.com>
2023-10-19 15:45:07 -0300

    creating confirmation modal

--- script.js
+++ script.js
@@ -53,6 +53,34 @@ navigator.geolocation.getCurrentPosition(
   }
 )
 
+//modal
+
+
+//Pegue o modal
+var modal = document.getElementById("my-modal");
+//Obtém o botão que abre o modal
+var btn = document.getElementById("open-modal");
+// Obtém o elemento <span> que fecha o modal
+var span = document.getElementsByClassName("close")[0];
+
+
+//Quando o usuário clicar no botão, abre o modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+  
+// Quando o usuário clicar em <span> (x), feche o modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+//Quando o usuário clicar em qualquer lugar fora do modal, feche-o
+window.onclick = function(event) {
+  if(event.target == modal){
+    modal.style.display = "none";
+  }
+}
+
+
 // Initialize and add the map
 let map
 
