fe919066f2b04a39a9d10fa2ded6ad916172e252
luiz <jpgpipinho@gmail.com>
2023-11-19 20:59:33 -0300

    att 0.8

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get("/presentes/", (req, res) => {
+router.get("/presentes/index.html", (req, res) => {
   const contatoPath = path.resolve(
     __dirname,
     "Menu",
