3ecf187d22f5324ee851a69531b1d13321b0b928
luiz <jpgpipinho@gmail.com>
2023-11-24 20:57:14 -0300

    Inserindo a API do googleSheets, criando rota para adicionar nome a tabela e criando fetch para inserir

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const Express = require("express");
 const path = require("path");
 const app = Express();
 const router = Express.Router();
+const { google } = require("googleapis");
 
 app.use(Express.static(path.join(__dirname, "Menu")));
+app.use(Express.json());
 
 router.get("/", (req, res) => {
   const indexPath = path.resolve(__dirname, "Menu", "index.html");
@@ -25,6 +27,47 @@ router.get("Presentes/index.html", (req, res) => {
   });
 });
 
+async function getAuthSheets() {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  const client = await auth.getClient();
+
+  const googleSheets = google.sheets({
+    version: "v4",
+    auth: client,
+  });
+
+  const spreadsheetId = "1Td-3lXLoZAfAUpDvDmHL8K87ZHdwGgYcJ5Ix36k7_Og";
+
+  return {
+    auth,
+    client,
+    googleSheets,
+    spreadsheetId,
+  };
+}
+
+app.post("/addRow", async (req, res) => {
+  const { googleSheets, auth, spreadsheetId } = await getAuthSheets();
+
+  const { values } = req.body;
+
+  const row = await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Página1",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: values,
+    },
+  });
+  res.send(row.data);
+});
+
+// app.post("/#", async);
 app.use(router);
 
 let port = process.env.PORT;
