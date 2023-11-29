6358e07d4c69c07caeb8c75ed387505257761dd9
luiz <jpgpipinho@gmail.com>
2023-11-29 15:06:35 -0300

    adicionando toast a aplicação

--- src/Menu/script1.js
+++ src/Menu/script1.js
@@ -21,29 +21,58 @@ window.onclick = function (event) {
   }
 };
 
+document.getElementById("my-btn").onclick = enviar;
+
 async function enviar() {
   const nome = document.getElementById("name").value;
   const data = {
     values: [[nome]],
   };
-  console.log(data);
-  try {
-    const response = await fetch("/addRow", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+  if (nome === "") {
+    console.log("O campo de nome está em branco.");
+  } else {
+    try {
+      const response = await fetch("/addRow", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-    if (response.ok) {
-      console.log("Linha adicionada com sucesso!");
-    } else {
-      console.error("Falha ao adicionar linha:", response.statusText);
+      if (response.ok) {
+        alert("Inserido com sucesso!");
+        showToast();
+      } else {
+        console.error("Falha ao adicionar linha:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Erro:", error.message);
     }
-  } catch (error) {
-    console.error("Erro:", error.message);
   }
 }
 
-document.getElementById("my-btn").onclick = enviar;
+function showToast() {
+  var toastContainer = document.getElementById("toast-container");
+
+  // Criar elemento de toast
+  var toast = document.createElement("div");
+  toast.className = "toast";
+  toast.textContent = "Esta é uma mensagem de toast!";
+
+  // Adicionar toast ao container
+  toastContainer.appendChild(toast);
+
+  // Animar o toast
+  setTimeout(function () {
+    toast.style.opacity = 1;
+  }, 10);
+
+  // Remover o toast após alguns segundos
+  setTimeout(function () {
+    toast.style.opacity = 0;
+    setTimeout(function () {
+      toastContainer.removeChild(toast);
+    }, 300);
+  }, 6000);
+}
